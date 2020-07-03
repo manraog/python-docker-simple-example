da4933a933423cf75b4826667fbc5fb006d6f451
Ricardo Ortega <ricardo.raog@gmail.com>
2020-07-03 09:14:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import math
 from flask import Flask
+
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print("Ok")
+    x = 0.0001
+    for i in range(1000000):
+        x = math.sqrt(x)
+    return "Load"    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
